30d08bba416e31e82f33c923a8c296cf8eca05f8
dolvin17 <huertas876@gmail.com>
2022-03-16 23:07:42 +0100

    ex07

---
+++ C02/ex07/ft_strupcase.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+char    *ft_strupcase(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i])
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+                str[i] = str[i] - 32;
+        i++;
+    }
+    return(str);
+}/*
+int main(void)
+{
+    char    str[] = "Psicine";
+    printf("%s\n", ft_strupcase(str));
+}*/
