6bd9201ead54a9b7ab5b18f587dbe71078d21ac0
dolvin17 <huertas876@gmail.com>
2022-03-16 23:07:55 +0100

    ex08

---
+++ C02/ex08/ft_str_lowcase.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+char    *ft_strlowcase(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+                str[i] = str[i] + 32;
+        i++;
+    }  
+    return(str);
+}
+int main(void)
+{
+    char str[] = "PISCINE";
+    printf("%s\n", ft_strlowcase(str));
+}
