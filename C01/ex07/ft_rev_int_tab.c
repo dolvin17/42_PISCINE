beb772b001fa749e6e6d6393a5b12511514e2a21
dolvin17 <huertas876@gmail.com>
2022-05-30 21:10:38 +0200

    ft_rev_int_tab

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -1,33 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 18:39:39 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/05/30 19:53:15 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
 #include <stdio.h>
-void    ft_rev_int_tab(int *tab, int size)
+
+void	ft_rev_int_tab(int *tab, int size)
 {
-    int temp;
-    int i;
+	int	i;
+	int	aux;
 
-    i = 0;
-    while(i < size / 2)
-    {
-         temp = tab[i];
-         tab[i] = tab[size - i - 1];
-         tab[size - i - 1] = temp;
-         i++;
-    }
+	i = 0;
+	while (i < size / 2)
+	{
+		aux = tab[i];
+		tab[i] = tab[size - i -1];
+		tab[size - i -1] = aux;
+		i++;
+	}
 }
-int main(void)
+/*
+int	main(void)
 {
-    int tab[]={0,1,2,3,4};
+	int	size;
+	int tab[]={0,1,2,3,4,5};
 
-    printf("before: %d\n", tab[0]);
-    printf("before: %d\n", tab[1]);
-    printf("before: %d\n", tab[2]);
-    printf("before: %d\n", tab[3]);
-    printf("before: %d\n", tab[4]);
-    ft_rev_int_tab(tab, 5);
-    printf("after: %d\n", tab[0]);
-    printf("after: %d\n", tab[1]);
-    printf("after: %d\n", tab[2]);
-    printf("after: %d\n", tab[3]);
-    printf("after: %d\n", tab[4]);
-
-    return (0);
-}
+	size = 6;
+	ft_rev_int_tab(tab, size);
+	printf("order: %d\n", tab[0]);
+	printf("order: %d\n", tab[1]);
+	printf("order: %d\n", tab[2]);
+	printf("order: %d\n", tab[3]);
+	printf("order: %d\n", tab[4]);
+	printf("order: %d\n", tab[5]);
+	return (0);
+}*/
