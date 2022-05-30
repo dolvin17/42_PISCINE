3e4a9c84853a0ca1a6352ecb5781ae8b7f129426
dolvin17 <huertas876@gmail.com>
2022-05-30 21:11:14 +0200

    ft_sort_int_tab

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -1,43 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 19:54:16 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/05/30 20:53:09 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
-void    ft_sort_int_tab(int *tab, int size)
+
+void	ft_sort_int_tab(int *tab, int size)
 {
-    int temp;
-    int i;
-    int j;
+	int	aux;
+	int	i;
+	int	j;
 
-    i = 0;
-    while(i < size)
-    {
-        j = i + 1;
-        while(j < size)
-        {
-            if(tab[i] > tab[j])
-            {
-                temp = tab[i];
-                tab[i] = tab[j];
-                tab[j] = temp;
-                i = 0;
-            }
-        ++j;    
-        }
-    i++;
-    }
+	i = 0;
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				aux = tab[i];
+				tab[i] = tab[j];
+				tab[j] = aux;
+				i = 0;
+			}
+		++j;
+		}
+	i++;
+	}
 }
-int main(void)
+/*
+int	main(void)
 {
-    int tab[]={4,0,3,1,2};
+	int	tab[]={4, 2, 5, 3, 1, 0};
+	int size;
 
-    printf("before: %d\n", tab[0]);
-    printf("before: %d\n", tab[1]);
-    printf("before: %d\n", tab[2]);
-    printf("before: %d\n", tab[3]);
-    printf("before: %d\n", tab[4]);
-    ft_sort_int_tab(tab, 5);
-    printf("after: %d\n", tab[0]);
-    printf("after: %d\n", tab[1]);
-    printf("after: %d\n", tab[2]);
-    printf("after: %d\n", tab[3]);
-    printf("after: %d\n", tab[4]);
-
-    return (0);
-}
+	size = 6;
+	ft_sort_int_tab(tab, size);
+	printf("%d\n", tab[0]);
+	printf("%d\n", tab[1]);
+	printf("%d\n", tab[2]);
+	printf("%d\n", tab[3]);
+	printf("%d\n", tab[4]);
+	printf("%d\n", tab[5]);
+	return (0);
+}*/
