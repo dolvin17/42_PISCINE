456fb8b89018114fe0d1d2399c362f6207c4cdeb
dolvin17 <huertas876@gmail.com>
2022-05-30 21:08:16 +0200

    ft_putstr

--- C01/ex05/ft_putstr.c
+++ C01/ex05/ft_putstr.c
@@ -1,21 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 17:39:57 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/05/30 18:21:55 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdio.h>
-void    ft_putstr(char *str)
-{
-    int i;
 
-    i = 0;
-    while(str[i] != '\0')
-    {
-        write(1, &str[i], 1);
-        i++;
-    }
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
 }
-int main(void)
-{   
-    char str;
-    
-    ft_putstr("What we do in the shadows");
-    printf("%s\n", &str);
-    return(0);
-}
+/*
+int	main(void)
+{
+	char	*str;
+
+	str = "Marvin";
+	printf("%s", str);
+	return (0);
+}*/
