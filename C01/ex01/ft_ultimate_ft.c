e3f0ee494ac876790f64be4f233895cac2c7ef94
dolvin17 <huertas876@gmail.com>
2022-05-30 21:04:01 +0200

    ft_ultimate_ft

--- C01/ex01/ft_ultimate_ft.c
+++ C01/ex01/ft_ultimate_ft.c
@@ -1,34 +1,48 @@
-#include <stdio.h>
-void    ft_ultimate_ft(int *********nbr)
-{
-    *********nbr = 42;
-}
-int main(void)
-{
-    int n;
-    int *********nbr;
-    int ********nbr2;
-    int *******nbr3;
-    int ******nbr4;
-    int *****nbr5;
-    int ****nbr6;
-    int ***nbr7;
-    int **nbr8;
-    int *nbr9;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 16:20:15 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/05/30 17:15:15 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    n = 3;
-    nbr9 = &n;
-    nbr8 = &nbr9;
-    nbr7 = &nbr8;
-    nbr6 = &nbr7;
-    nbr5 = &nbr6;
-    nbr4 = &nbr5;
-    nbr3 = &nbr4;
-    nbr2 = &nbr3;
-    nbr = &nbr2;
-   
-   printf("variable: %d\n", n);
-   ft_ultimate_ft(nbr);
-   printf("pointer: %d\n", n);
-   return (0);
+#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{	
+	*********nbr = 42;
 }
+/*
+int	main(void)
+{
+	int n;
+	int *********nbr;
+	int ********nbr2;
+	int *******nbr3;
+	int ******nbr4;
+	int *****nbr5;
+	int ****nbr6;
+	int ***nbr7;
+	int **nbr8;
+	int *nbr9;
+
+	n = 28;
+	nbr9 = &n;
+	nbr8 = &nbr9;
+	nbr7 = &nbr8;
+	nbr6 = &nbr7;
+	nbr5 = &nbr6;
+	nbr4 = &nbr5;
+	nbr3 = &nbr4;
+	nbr2 = &nbr3;
+	nbr = &nbr2;
+
+	printf("var: %d\n", n);
+	ft_ultimate_ft(nbr);
+	printf("pointer: %d\n", n);
+	return (0);
+}*/
