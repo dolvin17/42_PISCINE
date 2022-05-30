d48ec69f0319a611999b2e0bba1115190e9055c4
dolvin17 <huertas876@gmail.com>
2022-05-30 05:18:35 +0200

    ft_putchar

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -1,19 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/27 18:02:25 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/05/27 18:54:26 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
- void ft_print_alphabet(void)
+void	ft_print_alphabet(void)
 {
-	char a;
-	char z;
+	char	*s;
+	int		i;
 
-	a = 97;
-	z = 122;
-	while (a <= z)
-	{
-		write(1, &a, 1);
-		a++;
-	}	
+	i = 0;
+	s = "abcdefghijklmnopqrstuvwxyz";
+	while (s[i] != '\0')
+		write(1, &s[i++], 1);
 }
-int main(void)
+/*
+int	main(void)
 {
 	ft_print_alphabet();
-}
+	return(0);
+}*/
