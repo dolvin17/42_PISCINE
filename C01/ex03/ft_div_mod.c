13ec278585574e30a97bcb6551e85a00d29a79a1
dolvin17 <huertas876@gmail.com>
2022-05-30 21:07:25 +0200

    ft_div_mod

--- C01/ex03/ft_div_mod.c
+++ C01/ex03/ft_div_mod.c
@@ -3,29 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_div_mod.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Karol <grks_17@hotmail.com>                +#+  +:+       +#+        */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/14 20:55:34 by Karol             #+#    #+#             */
-/*   Updated: 2022/03/14 21:43:23 by Karol            ###   ########.fr       */
+/*   Created: 2022/05/30 17:16:30 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/05/30 17:24:39 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-void    ft_div_mod(int a, int b, int *div, int *mod)
-{
-    *div = a / b;
-    *mod = a % b;
-}
-int main(void)
-{
-    int a;
-    int b;
-    int div;
-    int mod;
 
-    a = 144;
-    b = 12;
-    ft_div_mod(a, b, &div, &mod);
-    printf("div:%d\nmod:%d\n", div, mod);
-    return(0);
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
 }
+/*
+int	main(void)
+{
+	int	a;
+	int	b;
+	int	mod;
+	int	div;
+
+	a = 144;
+	b = 12;
+	ft_div_mod(a, b, &div, &mod);
+	printf("div: %d\n", div);
+	printf("mod: %d\n", mod);
+	return (0);
+}*/
