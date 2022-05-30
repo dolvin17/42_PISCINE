d08337bd1e876ddb3f221dcee62d96d8862a7f33
dolvin17 <huertas876@gmail.com>
2022-05-30 21:07:54 +0200

    ft_ultimate_div_mod

--- C01/ex04/ft_ultimate_div_mod.c
+++ C01/ex04/ft_ultimate_div_mod.c
@@ -1,20 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 17:26:02 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/05/30 17:38:41 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
-void    ft_ultimate_div_mod(int *a, int *b)
-{
-    int temp;
 
-    temp = *a;
-    *a = *a / *b;
-    *b = temp % *b;
-}
-int main(void)
+void	ft_ultimate_div_mod(int *a, int *b)
 {
-    int a;
-    int b;
+	int	x;
+	int	y;
 
-    a = 144;
-    b = 12;
-    ft_ultimate_div_mod(&a, &b);
-    printf("div:%d\nmod:%d\n", a, b);
-    return (0);
+	x = *a / *b;
+	y = *a % *b;
+	*b = y;
+	*a = x;
 }
+/*
+int	main(void)
+{
+	int	x;
+	int	y;
+	int	a;
+	int	b;
+
+	a = 144;
+	b = 12;
+	ft_ultimate_div_mod(&a, &b);
+	printf("div: %d\n", a);
+	printf("mod: %d", b);
+	return (0);
+}*/
