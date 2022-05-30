e547ce236c8fbb014ef1452022ac845e1deaa54a
dolvin17 <huertas876@gmail.com>
2022-05-30 05:29:40 +0200

    ft_print_alphabet

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 18:02:25 by dolvin17          #+#    #+#             */
-/*   Updated: 2022/05/27 18:54:26 by dolvin17         ###   ########.fr       */
+/*   Updated: 2022/05/30 05:29:24 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	ft_print_alphabet(void)
 {
-	char	*s;
+	char	*str;
 	int		i;
 
 	i = 0;
-	s = "abcdefghijklmnopqrstuvwxyz";
-	while (s[i] != '\0')
-		write(1, &s[i++], 1);
+	str = "abcdefghijklmnopqrstuvwxyz";
+	while (str[i] != '\0')
+		write(1, &str[i++], 1);
 }
 /*
 int	main(void)
