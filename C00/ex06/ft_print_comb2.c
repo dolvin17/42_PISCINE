ab91778470e94716e25c2bc132ef7f89cae53b25
dolvin17 <huertas876@gmail.com>
2022-05-30 05:35:51 +0200

    ft_print_comb2

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -1,6 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/28 14:56:20 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/05/30 05:35:40 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void	show(int x);
+void	ft_source(int n)
+{
+	char	c;
+
+	c = n + '0';
+	write(1, &c, 1);
+}
+
+void	ft_print_source(int n)
+{
+	if (n <= 9)
+	{
+		ft_source(0);
+		ft_source(n);
+	}
+	else
+	{
+		ft_source(n / 10);
+		ft_source(n % 10);
+	}
+}
 
 void	ft_print_comb2(void)
 {
@@ -8,44 +40,23 @@ void	ft_print_comb2(void)
 	int	j;
 
 	i = 0;
-	while (i <= 99)
+	while (i <= 98)
 	{
 		j = i + 1;
-		while (j < 100)
+		while (j <= 99)
 		{
-			show(i);
+			ft_print_source(i);
 			write(1, " ", 1);
-			show(j);
+			ft_print_source(j);
 			if (!(i == 98 && j == 99))
-			{
 				write(1, ", ", 2);
-			}
 			j++;
 		}
 	i++;
 	}
 }
-
-void	show(int x)
+/*
+int	main(void)
 {
-	int	n1;
-	int	n2;
-
-	if (x > 9)
-	{
-		n1 = x / 10;
-		n2 = x % 10;
-		print(n1);
-		print(n2);
-	}
-	else
-	{
-		print(0);
-		print(x);
-	}
-}
-int main(void)
-{
-    ft_print_comb2 ();
-
-}
+	ft_print_comb2( );
+}*/
