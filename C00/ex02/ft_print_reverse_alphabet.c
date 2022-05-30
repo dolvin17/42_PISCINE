83dc6f51a3f2bde3af5d3f99ee799b92633f2d4c
dolvin17 <huertas876@gmail.com>
2022-05-30 05:31:57 +0200

    ft_print_reverse_alphabet

--- C00/ex02/ft_print_reverse_alphabet.c
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -6,12 +6,12 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 18:57:23 by dolvin17          #+#    #+#             */
-/*   Updated: 2022/05/27 23:15:16 by dolvin17         ###   ########.fr       */
+/*   Updated: 2022/05/30 05:31:40 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
+/*revés*/
 void	ft_print_reverse_alphabet(void)
 {
 	char		a;
