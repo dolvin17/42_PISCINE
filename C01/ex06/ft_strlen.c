d4af8787bf8aeba04e27d4a428c99255fc6fd127
dolvin17 <huertas876@gmail.com>
2022-05-30 21:08:57 +0200

    ft_strlen

--- C01/ex06/ft_strlen.c
+++ C01/ex06/ft_strlen.c
@@ -1,17 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 18:23:33 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/05/30 18:37:36 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
-int ft_strlen(char *str)
+
+int	ft_strlen(char *str)
 {
-    int i;
-    
-    i = 0;
-    while (str[i] != '\0')
-    {
-        i++;
-    }
-        return(i);
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
 }
-int main(void)
+/*
+int	main(void)
 {
-    printf("%d\n", ft_strlen("42piscinefeb2022"));
-    return(0);
-}
+	char	*str;
+	
+	str = "las tildes cuentan como chars";
+	printf("%d", ft_strlen(str));
+	return (0);
+}*/
