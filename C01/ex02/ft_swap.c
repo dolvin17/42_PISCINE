38869b891990cecf87c1a1e36af4ba782a909324
dolvin17 <huertas876@gmail.com>
2022-05-30 21:04:30 +0200

    ft_swap

--- C01/ex02/ft_swap.c
+++ C01/ex02/ft_swap.c
@@ -1,21 +1,35 @@
-#include <stdio.h>
-void    ft_swap(int *a, int *b)
-{
-    int x;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 16:59:48 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/05/30 17:14:29 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    x = *a;
-    *a = *b;
-    *b = x;
-}
-int main(void)
-{
-    int a;
-    int b;
+#include <printf.h>
 
-    a = 42;
-    b = 90;
-    printf("before: a=%d b=%d\n", a, b);
-    ft_swap(&a, &b);
-    printf("after: a=%d b=%d\n", a, b);
-    return(0);
+void	ft_swap(int *a, int *b)
+{
+	int	x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
 }
+/*
+int	main(void)
+{
+	int a;
+	int b;
+
+	a = 42;
+	b = 666;
+	printf("Variable: a = %d b = %d\n", a, b);
+	ft_swap(&a, &b);
+	printf("Pointer: a = %d b = %d\n", a, b);
+	return (0);
+}*/
