29041cf88900a1c8b60fddc0d15a7e2d40f83cc9
dolvin17 <huertas876@gmail.com>
2022-05-31 06:18:48 +0200

    ft_str_is_numeric

--- C02/ex03/ft_str_is_numeric.c
+++ C02/ex03/ft_str_is_numeric.c
@@ -1,23 +1,40 @@
-#include <stdio.h>
-int ft_str_is_numeric(char *str)
-{
-    int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 05:21:56 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/05/31 06:10:59 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    while(str[i])
-    {
-        if(str[i] >= '0' && str[i] <= '9')
-                ;
-        else
-                return(0);
-        i++;
-    }
-    return(1);
-}/*
-int main(void)
+#include <stdio.h>
+
+int	ft_str_is_numeric(char *str)
 {
-    printf("%d\n", ft_str_is_numeric("123213"));
-    printf("%d\n", ft_str_is_numeric("abcf"));
-    printf("%d\n", ft_str_is_numeric(""));
-    return(0);
-}/*
+	int	n;
+	int	n2;
+
+	n = 0;
+	n2 = 9;
+	while (str[n])
+	{
+		if (str[n] >= '0' && str[n] <= '9')
+			;
+		else
+			return (0);
+		n++;
+	}
+	return (1);
+}
+/*
+int	main(void)
+{
+	char	*str;
+
+	str = "0123456789";
+
+	ft_str_is_numeric(str);
+}*/
