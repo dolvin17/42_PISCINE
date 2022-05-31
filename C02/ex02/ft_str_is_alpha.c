9a4e8b10f7dcc01c1a0dba95f4be8dd42cc8ea7b
dolvin17 <huertas876@gmail.com>
2022-05-31 06:18:38 +0200

    ft_str_is_alpha

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -1,24 +1,46 @@
-#include <stdio.h>
-int ft_str_is_alpha(char *str)
-{
-    int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 02:00:45 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/05/31 05:18:45 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] >= 'A' && str[i] <= 'Z')
-                ;
-        else if (str[i] >= 'a' && str[i] <= 'z')
-                ;
-        else
-            return(0);
-        ++i;
-    }
-    return(1);
-}/*
-int main(void)
+#include <stdio.h>
+
+int	ft_str_is_alpha(char *str)
 {
-    printf("%d\n", ft_str_is_alpha("ABC"));
-    printf("%d\n", ft_str_is_alpha("9695"));
-    printf("%d\n", ft_str_is_alpha(""));
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			;
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			;
+		else
+			return (0);
+	++i;
+	}
+	return (1);
+}
+/*
+int	main(void)
+{
+	char	*str;
+	char	*str1;
+	char	*str2;
+
+	str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	str1 = "";
+	str2 = "NULL";
+	printf("%d\n", ft_str_is_alpha(str));
+	printf("%d\n", ft_str_is_alpha(str1));
+	printf("%d\n", ft_str_is_alpha(str2));
+	return (0);
 }*/
