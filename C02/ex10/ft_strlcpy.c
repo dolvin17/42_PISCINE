24e3469f06a1ad2255f82ad2f5a9bca7dc01da6e
dolvin17 <huertas876@gmail.com>
2022-06-02 15:40:33 +0200

    ft_strlcpy

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -1,5 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/02 00:20:57 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/06/02 15:33:48 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 unsigned int	ft_strlen(char *str)
 {
@@ -11,16 +24,18 @@ unsigned int	ft_strlen(char *str)
 	return (len);
 }
 
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int dstsize)
 {
 	unsigned int	i;
+	unsigned int	buffer;
 
-	if (size < 1)
+	buffer = dstsize - 1;
+	if (dstsize < 1)
 		return (ft_strlen(src));
 	i = 0;
-	while (src[i] != '\0' && i < (size - 1))
+	while (src[i] != '\0' && i < buffer)
 	{
-		dest[i] = src [i];
+		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
@@ -28,11 +43,15 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	return (i);
 }
 /*
-int main()
+int		main(void)
 {
-    char    src[] = "MARVIN";
-    char    dest[] = "ESMIAMOR";
-    unsigned int    size = 3;
-    printf("%i\n", ft_strlcpy(dest, src, size));
-    return (0);
+	char	dest[97];
+	char	*src;
+	int		size;
+
+	src = "Marvin es mi amor";
+	size = 40;
+	printf("%d\n", ft_strlcpy(dest, src, size));
+	printf("%s\n\n", dest);
+	printf("%s\n\n", dest);
 }*/
