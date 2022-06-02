79a6a7203fa6af7e69aaf03cf9a085f31b1f1798
dolvin17 <huertas876@gmail.com>
2022-06-02 15:39:26 +0200

    ft_str_is_lowercase

--- C02/ex04/ft_str_is_lowercase.c
+++ C02/ex04/ft_str_is_lowercase.c
@@ -1,25 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_lowercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 18:18:05 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/05/31 20:58:39 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
-int ft_str_is_lowercase(char *str)
+
+int	ft_str_is_lowercase(char *str)
 {
-    int i;
-    
-    i = 0;
-    while(str[i])
-    {
-        if(str[i] >= 'a' && str[i] <= 'z')
-                ;
-        else
-            return(0);
-        i++;           
-    }
-    return(1);
-}/*
-int main(void)
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (1);
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 'a' && str[i] <= 'z'))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+/*
+int	main(void)
 {
-    printf("%d\n", ft_str_is_lowercase("abcd"));
-    printf("%d\n", ft_str_is_lowercase("ABC"));
-    printf("%d\n", ft_str_is_lowercase("01234"));
-    printf("%d\n", ft_str_is_lowercase("    "));
-    printf("%d\n", ft_str_is_lowercase(""));
-    return(0);
-}*/
+	char	*str;
+	char	*str1;
+	char	*str2;
+
+	str = "amarvin";
+	str1 = "leonardo1";
+	str2 = "·$%$·&/·&/";
+	printf("%d\n", ft_str_is_lowercase(str));
+	printf("%d\n", islowercase(str));
+	printf("%d\n", ft_str_lowercase(str1));
+	printf("%d\n", islowercase(str1));
+	printf("%d\n", ft_str_is_lowercase(str2));
+	printf("%d\n", islowercase(str2));
+	return (0);
+}
+*/
