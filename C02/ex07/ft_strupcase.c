982385357b8f17b9a6f6a2cb73e9f2e216cd6ca9
dolvin17 <huertas876@gmail.com>
2022-06-02 15:39:58 +0200

    ft_strupcase

--- C02/ex07/ft_strupcase.c
+++ C02/ex07/ft_strupcase.c
@@ -1,19 +1,45 @@
-#include <stdio.h>
-char    *ft_strupcase(char *str)
-{
-    int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 21:11:41 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/06/01 00:12:38 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    while(str[i])
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-                str[i] = str[i] - 32;
-        i++;
-    }
-    return(str);
-}/*
-int main(void)
+#include <stdio.h>
+
+char	*ft_strupcase(char *str)
 {
-    char    str[] = "Psicine";
-    printf("%s\n", ft_strupcase(str));
-}*/
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
+/*
+int	main(void)
+{
+	char	*str;
+	char	*str1;
+	char	*str2;
+
+	str = "marvin";
+	str1 = "leonardo1";
+	str2 = "·$%$·&/·&/";
+	printf("%s\n", ft_strupcase(str));
+	printf("%s\n", ft_strupcase(str1));
+	printf("%s\n", ft_strupcase(str2));
+	return (0);
+}
+*/
