93841a7099e27215465045b935b988e69f54e7aa
dolvin17 <huertas876@gmail.com>
2022-06-02 15:40:19 +0200

    ft_strcapitalize

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -1,46 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/01 00:25:51 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/06/02 00:10:59 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
-int is_alphanumeric(char c)
-{
-    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'));
 
-}
-int is_lower(char c)
+char	*ft_strcapitalize(char *str)
 {
-    return (c >= 'a' && c <= 'z');
-}
-char    *ft_strlowcase(char *str)
-{
-    int i;
+	int	i;
+	int	j;
 
-    i = 0;
-    while(str[i])
-    {
-        if (str[i] >= 'A' && str[i] <= 'Z')
-            str[i] -= ('A' - 'a');
-        i++;
-    }
-    return(str);
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
+	j = 0;
+	i = 1;
+	while (str[i] != '\0')
+	{
+		if (!((str [j] >= 'a' && str[j] <= 'z')
+				|| (str[j] >= 'A' && str[j] <= 'Z')
+				|| (str[j] >= '0' && str[j] <= '9')))
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+					str[i] -= 32;
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+		j++;
+	}
+	return (str);
 }
-char    *ft_strcapitalize(char *str)
+/*
+int	main(void)
 {
-    int i;
-
-    i = 0;
-    ft_strlowcase(str);
-    if (is_lower(str[i]))
-        str[i] -= 32;
-    while (str[i++])
-    {
-        if (!is_alphanumeric(str[i]) && is_lower(str[i + 1]))
-            str[i + 1] -= 32;
-    }
-    return(str);
-}
-int main(void)
-{
-    char    test1[] = "salut, comment tu vas ? 42mots quaRante-deux; cinquante+et+un";
-	char	test2[] = "		Hello, 7what &&is/going     ";
-    printf("%s\n", ft_strcapitalize(test1));
-    printf("%s\n", ft_strcapitalize(test2));
-    return (0);
-}
+	char	str[] = " gatito, bonito, paquito42-leonc;:!?$&/()ito";
+	printf("%s\n", ft_strcapitalize(str));
+	return (0);
+}*/
